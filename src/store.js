b83e35e271c855f6ee0e2d5aee6c3e656013cf17
Lars Nolden <larsnolden@web.de>
2018-07-10 09:29:34 -0400

    added mutations to store

--- src/store.js
+++ src/store.js
@@ -8,9 +8,43 @@ export default new Vuex.Store({
     todos: [{
       id: 0,
       title: 'One',
-      complete: false
+      complete: false,
+      archived: true
     }]
   },
-  mutations: {},
+  mutations: {
+    addTodo (state, { title }) {
+      const todo = {
+        title,
+        id: state.todos.length + 1,
+        complete: false,
+        archived: true
+      }
+      state.todos = [...state.todos, todo]
+    },
+    toggleComplete (state, { id }) {
+      state.todos = state.todos.map(todo => {
+        if (todo.id === id) return { ...todo, complete: !todo.complete }
+        return todo
+      })
+    },
+    archive (state, { id }) {
+      state.todos = state.todos.map(todo => {
+        if (todo.id === id) return { ...todo, archived: true }
+        return todo
+      })
+    },
+    delete (state, { id }) {
+      state.todos = state.todos.filter(todo => todo.id !== id)
+    }
+  },
+  getters: {
+    todos (state) {
+      return state.todos.filter(todo => !todo.archived)
+    },
+    archivedTodos (state) {
+      return state.todos.filter(todo => todo.archived)
+    }
+  },
   actions: {}
 })
